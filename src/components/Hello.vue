0fbe6e7d53cd55d9cfef798b740dbdee0328c37c
eric <github@ericjj.com>
2017-09-19 13:54:17 +0800

    no message

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -47,8 +47,6 @@
       </section>
       <footer class="info">
       <p>Double-click to edit a todo</p>
-      <p>Written by <a href="http://evanyou.me">Evan You</a></p>
-      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
       </footer>
   </div>
 </template>
